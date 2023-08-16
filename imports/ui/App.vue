5ba986c068649afd43b8259b257feb50039742b8
keela <harublank00@gmail.com>
2023-08-16 15:00:45 +0545

    initial project setup and make a login page

---
+++ imports/ui/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div class="p-8">
+    <router-view />
+  </div>
+</template>
---
+++ imports/ui/Login.page.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { reactive } from 'vue';
+
+const userCredendials = reactive({
+    email: "",
+    password: ""
+})
+
+
+const handleSubmit = () => {
+    const { email, password } = userCredendials
+    console.log({ email, password })
+}
+
+</script> 
+
+<template>
+    <form class="max-w-xs" @submit.prevent="handleSubmit">
+        <div>
+            <input class="w-full border mb-2 p-1" type="email" placeholder="johndoe@keela.com" required
+                v-model="userCredendials.email">
+        </div>
+
+        <div>
+            <input class="w-full border mb-2 p-1 " type="password" placeholder="fdfUIUI3#$#$#5" required
+                v-model="userCredendials.password" />
+        </div>
+
+        <button class="bg-blue-700 text-white px-2 py-1 w-full" type="submit">
+            login
+        </button>
+    </form>
+</template>
